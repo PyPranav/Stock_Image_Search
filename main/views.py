1c651e253618832bcc3e154cd47dbfb37671b838
PyPranav <59741576+PyPranav@users.noreply.github.com>
2022-09-19 05:35:24 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,11 +9,12 @@ def image(Responce):
     return render(Responce, 'image_search_home.html')
 
 def results(Responce):
-    if Responce.method == "POST":
-        query = Responce.POST.get('search-box')
+    if Responce.method == "GET":
+        query = Responce.GET.get('search-box')
         print(query)
         r = requests.get(f'https://pixabay.com/api/?key={API_KEY}&q={query}&image_type=photo&per_page=40&safesearch=true&orientation=horizontal').json()
         with open('static/images.json', "w") as f:
             json.dump(r['hits'], f)
         return render(Responce, 'image_search_result.html')
+    
     
